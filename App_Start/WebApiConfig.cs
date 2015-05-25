41924a73dd1a7cde2e87ccbb55aa61a34f6df67a
Justin Boyd <Justin@boyd.net.au>
2015-05-25 16:45:55 +1000

    auto restore nuget packages

--- App_Start/WebApiConfig.cs
+++ App_Start/WebApiConfig.cs
@@ -17,22 +17,6 @@ namespace BSBChecker
                 routeTemplate: "{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
-            config.Formatters.Add(new BrowserJsonFormatter());
-        }
-
-        public class BrowserJsonFormatter : JsonMediaTypeFormatter
-        {
-            public BrowserJsonFormatter()
-            {
-                this.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
-                this.SerializerSettings.Formatting = Formatting.Indented;
-            }
-
-            public override void SetDefaultContentHeaders(Type type, HttpContentHeaders headers, MediaTypeHeaderValue mediaType)
-            {
-                base.SetDefaultContentHeaders(type, headers, mediaType);
-                headers.ContentType = new MediaTypeHeaderValue("application/json");
-            }
         }
     }
 }
